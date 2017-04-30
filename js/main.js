c9e8d3a14040cda763a5d1aa27bcede3bedc273c
Damian Fiałkiewicz <aekal4@gmail.com>
2017-04-30 15:13:54 +0200

    Add markers based on json info

--- js/main.js
+++ js/main.js
@@ -1,5 +1,37 @@
 var map;
-function initMap() {
+
+$(function() {
+	//Get places info from json file
+	$.getJSON("js/places.json", function(data) {
+		var avgLat, avgLng, mapPos, markerPos, markerPosArray;
+		mapPos = 0;
+		avgLat = 0;
+		avgLng = 0;
+		markerPosArray = [];
+		//Set markers position
+		for (var i = 0; i < data.dominos.length; i++) {
+			markerPos = {
+				lat: data.dominos[i].lat,
+				lng: data.dominos[i].lng
+			}
+			markerPosArray.push(markerPos);
+			avgLat += markerPos.lat;
+			avgLng += markerPos.lng;
+		}
+		//Set average position for center map
+		mapPos = {
+			lat: avgLat/data.dominos.length,
+			lng: avgLng/data.dominos.length
+		}
+		initMap(mapPos, markerPosArray);
+	});
+});
+
+function initMap(mapPos, markerPosArray) {
+	createMap(mapPos);
+	for (var i = 0; i < markerPosArray.length; i++) {
+		createMarker(markerPosArray[i]);
+	};
 	var styles = [
 		{
 			featureType: "road",
@@ -10,42 +42,18 @@ function initMap() {
 			]
 		}
 	];
-
-	map = new google.maps.Map(document.getElementById("map"), {
-		center: {lat: 36.8, lng: -83},
-    zoom: 6,
-		disableDefaultUI: true,
-		styles: styles
-	});
-
-	
-	var marker = new google.maps.Marker({
-		position: {lat: 38.9658157, lng: -84.6743774},
-		map: map
-	});
-
-	var marker = new google.maps.Marker({
-		position: {lat: 36.0602014, lng: -88.1158447},
-		map: map
-	});
-
-	var marker = new google.maps.Marker({
-		position: {lat: 34.0958849, lng: -86.8016052},
-		map: map
-	});
-	var marker = new google.maps.Marker({
-		position: {lat: 35.3722548, lng: -80.1974487},
-		map: map
-	});
-
-	var marker = new google.maps.Marker({
-		position: {lat: 36.1012669, lng: -78.4657288},
-		map: map
-	});
-
-
-
-
+	function createMap (mapPos) {
+		map = new google.maps.Map(document.getElementById("map"), {
+			center: mapPos,
+			zoom: 5,
+			disableDefaultUI: true,
+			styles: styles
+		});
+	}
+	function createMarker (markerPos) {
+		new google.maps.Marker({
+			position: markerPos,
+			map: map
+		});
+	}
 }
-
-//ChIJoU6wE7zGQYgRJNDXqvgDTIQ
